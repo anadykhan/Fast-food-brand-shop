3dad992b756d4c4efe92de4ffe99cfd13118aa2e
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2023-10-25 22:57:35 +0600

    Third slider image bug flixed

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -62,7 +62,7 @@ const ProductDetails = () => {
                         </div>
                     </div>
                     <div id="slide3" className="carousel-item relative w-full">
-                        <img src="/https://i.ibb.co/WsggmgZ/Screenshot-2023-10-25-at-10-55-56-PM.png" className="w-full" />
+                        <img src="https://i.ibb.co/WsggmgZ/Screenshot-2023-10-25-at-10-55-56-PM.png" className="w-full" />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide2" className="btn btn-circle">❮</a>
                             <a href="#slide4" className="btn btn-circle">❯</a>
